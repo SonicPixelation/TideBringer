438ed532c08f977476176a9e57b8646ef774c7ba
SonicPixelation <sonicpixelation@gmail.com>
2017-04-04 16:50:41 -0400

    version 0.0.1a
    updated version number to 0.0.1a

--- src/dev/sonicpixelation/tidebringer/TideVersion.java
+++ src/dev/sonicpixelation/tidebringer/TideVersion.java
@@ -3,7 +3,7 @@ package dev.sonicpixelation.tidebringer;
 
 public class TideVersion {
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.1a";
 
     private TideVersion(){}
 }
